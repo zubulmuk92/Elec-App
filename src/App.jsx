8987dc46e3bb50d5343fe943609b43c822fbbaef
zubulmuk92 <73728454+zubulmuk92@users.noreply.github.com>
2023-04-17 14:52:15 +0200

    remove routers for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
 import React from "react"
 import { Presentation, Resistances, Choix } from "./components"
 
-import { BrowserRouter } from "react-router-dom"
-import { Routes,Route } from "react-router-dom"
-
 const App = () => {
   return (
     <>
-    <BrowserRouter>
       <div className="bg-cover bg-no-repeat bg-center bg-bg-elec">
         <Presentation />
       </div>
 
-      <Routes>
-        <Route path="/" element={
-          <div className="bg-[#03071e]">
-            <Resistances />
-          </div> 
-        } />
-        <Route path="/resistances" element={
-          <div className="bg-[#03071e]">
-            <Resistances />
-          </div> 
-        } />
-      </Routes>
-
-    </BrowserRouter>
+    
+      <div className="bg-[#03071e]">
+        <Resistances />
+      </div> 
     </>
   )
 }
